6cce8f4572f30b0a63bf60d6c01ed96e3e6bf06a
ndamukunda139 <37266165+ndamukunda139@users.noreply.github.com>
2025-05-27 16:16:39 +0000

    finalDeploy model

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import matplotlib.pyplot aselt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import shap
 from PIL import Image
